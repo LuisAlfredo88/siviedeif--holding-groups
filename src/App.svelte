0fd509da9b0ef1faeaae98362d49044f23e06ab4
ldilone <luisalfredodilone@gmail.com>
2021-02-15 22:51:27 -0400

    Loading project

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<svelte:head>
+  <style src="./assets/css/style.scss"></style>
+</svelte:head>
+
+<main>
+	<h1>First Load {name}</h1>
+</main>
