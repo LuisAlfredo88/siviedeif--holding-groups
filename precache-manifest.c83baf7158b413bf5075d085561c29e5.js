e5fce0c91e433c5b007ef74eb2d6b5047bbbd95e
ldilone <luisalfredodilone@gmail.com>
2021-02-19 17:02:24 -0400

    test

---
+++ precache-manifest.c83baf7158b413bf5075d085561c29e5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b769ef2125c4cac8b244",
+    "url": "/siviedeif--holding-groups/css/app.0bc5736f.css"
+  },
+  {
+    "revision": "f496512b33503d070b2eb2942adf8bf6",
+    "url": "/siviedeif--holding-groups/img/landing/cli.png"
+  },
+  {
+    "revision": "9ed3fec3a6fe560dc78cd31717cd4e99",
+    "url": "/siviedeif--holding-groups/img/landing/cloud.png"
+  },
+  {
+    "revision": "c300cf8185865795cb394ef73cab1ae0",
+    "url": "/siviedeif--holding-groups/img/landing/docs.png"
+  },
+  {
+    "revision": "3838038413ecd841928b59c6712962e0",
+    "url": "/siviedeif--holding-groups/img/landing/favicon.png"
+  },
+  {
+    "revision": "f00a7efa141490b1ef4a96854b8d01ba",
+    "url": "/siviedeif--holding-groups/img/landing/logo.jpg"
+  },
+  {
+    "revision": "9d57a82ec451fbc172c4d50cad0e9cc8",
+    "url": "/siviedeif--holding-groups/img/landing/logo.png"
+  },
+  {
+    "revision": "3967bb70bd3fca6b331c39ad4b02bcde",
+    "url": "/siviedeif--holding-groups/img/landing/logos/clojure.png"
+  },
+  {
+    "revision": "4c48bffa9426ec3916091de68964b995",
+    "url": "/siviedeif--holding-groups/img/landing/logos/csharp.png"
+  },
+  {
+    "revision": "3ea7de99e0bdc3703d0017875ffb5927",
+    "url": "/siviedeif--holding-groups/img/landing/logos/go.png"
+  },
+  {
+    "revision": "e198a02a09bbec58113d3d25a6335bb3",
+    "url": "/siviedeif--holding-groups/img/landing/logos/node.png"
+  },
+  {
+    "revision": "caaf73f7dbb0b1ad4af64eb5f882e446",
+    "url": "/siviedeif--holding-groups/img/landing/logos/php.png"
+  },
+  {
+    "revision": "49539b11e418f358090208c9cc535fa7",
+    "url": "/siviedeif--holding-groups/img/landing/logos/python.png"
+  },
+  {
+    "revision": "f2e1af8cf1c36efd91cef9252c402bca",
+    "url": "/siviedeif--holding-groups/img/landing/logos/ruby.png"
+  },
+  {
+    "revision": "38bbfffe714d280312a26e88bddc4796",
+    "url": "/siviedeif--holding-groups/img/landing/logos/scala.png"
+  },
+  {
+    "revision": "a609c21aef0d0028c0dabef4bd70fbbe",
+    "url": "/siviedeif--holding-groups/img/landing/npm.png"
+  },
+  {
+    "revision": "9a37db31a56d5ac563a61cada0895bbb",
+    "url": "/siviedeif--holding-groups/img/landing/server.png"
+  },
+  {
+    "revision": "1b24f9d45c54b021fae67cb1c2eddca1",
+    "url": "/siviedeif--holding-groups/img/landing/server2.png"
+  },
+  {
+    "revision": "ada01f74d250d85ce153b9a9aa0f6413",
+    "url": "/siviedeif--holding-groups/index.html"
+  },
+  {
+    "revision": "b769ef2125c4cac8b244",
+    "url": "/siviedeif--holding-groups/js/app.42d04b4d.js"
+  },
+  {
+    "revision": "4dc24d4bf73c0ce7c79c",
+    "url": "/siviedeif--holding-groups/js/chunk-2d21dfc8.a20b87df.js"
+  },
+  {
+    "revision": "77b8e72097371b728475",
+    "url": "/siviedeif--holding-groups/js/chunk-vendors.de0f7a1e.js"
+  },
+  {
+    "revision": "efe894a28bb61b912612c0c880988e91",
+    "url": "/siviedeif--holding-groups/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/siviedeif--holding-groups/robots.txt"
+  }
+]);
